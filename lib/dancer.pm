99cab9d5c56b491f14774b3b781e488b69ba18d8
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-01-11 20:46:33 +0100

    Add blog tags

--- lib/dancer.pm
+++ lib/dancer.pm
@@ -3,10 +3,55 @@ use Dancer ':syntax';
 use Text::Markdown;
 use Path::Class qw( file dir );
 use Template;
+use File::Find;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
+sub parse_blog_entries {
+    my $dir = shift;
+
+    my @entries;
+    find(
+        sub {
+
+            # we only care about blog entries
+            return if !/\.md$/;
+
+            # get a Path::Class::File for it
+            my $file = file($File::Find::name);
+            my $fh   = $file->openr;
+
+            # parse a simple header using the kite secret operator
+            chomp( my ( $title, $date, $tags ) =
+                  ( ~~ <$fh>, ~~ <$fh>, ~~ <$fh> ) );
+            $title =~ s/^[#\s]+//;
+            $date  =~ s/^[#\s]+//;
+            $tags  =~ s/^[#\s]+//;
+
+            # update the structure will all relevant information
+            my $source = substr( $File::Find::name, length($dir) );
+            ( my $url = $source ) =~ s/\.md$/.html/;
+
+            push @entries,
+              {
+                url    => '.' . $url,
+                title  => $title,
+                date   => $date,
+                tags   => [ split /\s*,\s*/, $tags ],
+                source => "$dir/$_",
+              };
+        },
+
+        #setting( 'public' )
+        $dir
+    );
+
+    return @entries;
+}
+
 get '/' => sub {
+
     #return "baf!";
     template 'about';
 };
@@ -22,30 +67,58 @@ get '/private' => sub {
 #
 # blog
 #
-my $blog_dir = dir( setting('public'), 'blog');
+
+# blog main page
+#
+my $blog_dir = dir( setting('public'), 'blog' );
 my $ext = 'md';
 
 get '/blog' => sub {
     my %links;
-    for my $file ( glob "$blog_dir/*.$ext" ) {
-        my ($name) = $file =~ /\/(\w+)\.$ext$/;
-        my $link = $name;
-        $link =~ s/^(.*)$/blog\/$1.html/;
-        $links{$name} = $link;
+    my @entries = parse_blog_entries($blog_dir);
+
+    my @tags;
+    for my $entry (@entries) {
+        for my $tag ( @{ $entry->{tags} } ) {
+            push @tags, $tag unless grep $tag eq $_, @tags;
+        }
     }
+
+    for my $entry (@entries) {
+        $entry->{url} =~ s/(.*)/blog\/$1/;
+    }
+
     set template => 'template_toolkit';
-    template 'blog1', { links => \%links };
+    template 'blog', { tags => \@tags, entries => \@entries };
 };
 
+# blog tags
 #
+get qr{/blog/(\w+)$} => sub {
+    my ($tag) = splat;
+    my @entries = parse_blog_entries($blog_dir);
+
+    my @tagged_entries;
+    for my $entry (@entries) {
+        for my $entry_tag ( @{ $entry->{tags} } ) {
+            if ( $entry_tag eq $tag ) {
+                push @tagged_entries, $entry
+                  unless grep $entry->{source} eq $_->{source}, @tagged_entries;
+            }
+        }
+    }
+
+    template 'blog_tags', { entries => \@tagged_entries };
+};
+
 # blog entries
 #
 my $m = Text::Markdown->new;
 
 get qr{/blog/(.*)\.html} => sub {
     my ($file) = splat;
-    my $text = file( $blog_dir, "$file.$ext")->slurp;
-    template 'blog', { content => $m->markdown($text) };
+    my $text = file( $blog_dir, "$file.$ext" )->slurp;
+    template 'blog_entry', { content => $m->markdown($text) };
 };
 
 true;
